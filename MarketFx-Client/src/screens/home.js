cb32dafda1f2d110e4391e2a95875652048784c0
hammadcreators <comsats@CL6-7.students.comsats.edu.pk>
2023-03-15 17:10:05 +0500

    Some improvements

--- MarketFx-Client/src/screens/home.js
+++ MarketFx-Client/src/screens/home.js
@@ -180,17 +180,35 @@ const Left = styled.div`
 
 const Home = () => {
   const [fetchedNews, setFetchedNews] = useState([]);
+  const [tab, setTab] = useState("economy");
+
+
+  const setTabHandler = async (tab) => {
+    setTab(tab);
+    await fetchNews(tab)
+  }
+
+
+  const fetchNews = async (term) => {
+    try {
+      setFetchedNews([]);
+      const response = await NewsApi.get(`/everything?q=${term}&pageSize=10`);
+      setFetchedNews(response.articles);
+    } catch (ex) {
+      alert(ex)
+    }
+  };
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await NewsApi.get(`/everything?q=bitcoin`);
+        const response = await NewsApi.get(`/everything?q=economy&pageSize=10`);
         setFetchedNews(response.articles);
       } catch (ex) {
         alert(ex)
       }
     })();
-  })
+  }, [])
   return (
     <div>
       <div className="Header">
@@ -272,18 +290,22 @@ const Home = () => {
 
             {/*  */}
             <TabsContainer>
-              <Tab active={true}>All News</Tab>
-              <Tab>Old News</Tab>
+              <Tab active={tab == "economy" ? true : false} onClick={() => setTabHandler("economy")}>All News</Tab>
+              <Tab active={tab == "gold" ? true : false} onClick={() => setTabHandler("gold")}>Gold News</Tab>
 
-              <Tab>Crude Oil News</Tab>
+              <Tab active={tab == "crude oil" ? true : false} onClick={() => setTabHandler("crude oil")}>Crude Oil News</Tab>
 
-              <Tab>Forex News</Tab>
+              <Tab active={tab == "forex" ? true : false} onClick={() => setTabHandler("forex")}>Forex News</Tab>
             </TabsContainer>
 
-            {fetchedNews.map((news) => {
-              return <NewsComponent news={news} />;
-            })}
 
+            {fetchedNews ?
+
+              fetchedNews.map((news) => {
+                return <NewsComponent news={news} />;
+              })
+
+              : null}
 
             <AppButton />
           </Left>
