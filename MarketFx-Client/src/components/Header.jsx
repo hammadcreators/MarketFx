50c67c06941cab4611b31bc190a79f99febc579e
hammadcreators <hammadali648@gmail.com>
2023-03-15 22:42:00 -0700

    Done with the navbar but it is not response yet we will work on it wehne we are done with main modules

---
+++ MarketFx-Client/src/components/Header.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form'
+import Container from 'react-bootstrap/Container';
+import Logo from "../assets/images/logo-transparent.png";
+import { Col, Row } from 'react-bootstrap';
+import styled from 'styled-components';
+import { FaAngleDown, FaSearch } from 'react-icons/fa';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+const Header = () => {
+
+
+
+  const StyledContainer = styled(Container)`
+    background-color: #3b4859;
+    padding: 10px 0px;
+    margin-top: 0px;
+    color: #eee;
+    text-align: center;
+  `;
+
+
+  const StyledNavbar = styled(Navbar)`
+    height: 70px;
+  `;
+
+
+  const DownArrow = styled.p`
+    font-size: 30px;
+    margin-left: 7px;
+  `;
+
+
+  const StyledCol = styled(Col)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  `;
+  return (
+    <>
+      <StyledNavbar className='shadow-0' expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={Logo}
+              width="100"
+              height="100"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#login">LOGIN</Nav.Link>
+              <Nav.Link href="#register">REGISTER</Nav.Link>
+            </Nav>
+
+            <Form className='d-flex'>
+              <InputGroup>
+                <Form.Control
+                  placeholder="Search..."
+                  aria-label="Search ..."
+                />
+                <InputGroup.Text id="basic-addon2"><FaSearch></FaSearch></InputGroup.Text>
+              </InputGroup>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </StyledNavbar>
+
+      <StyledContainer fluid>
+        <Row className="w-75 m-auto">
+          <StyledCol>Market News  <FaAngleDown/> </StyledCol>
+          <StyledCol>Analysis News <FaAngleDown/></StyledCol>
+          <StyledCol>Curriencies <FaAngleDown/></StyledCol>
+          <StyledCol>Stocks <FaAngleDown/> </StyledCol>
+          <StyledCol>Crypto <FaAngleDown/> </StyledCol>
+
+        </Row>
+
+      </StyledContainer>
+
+    </>
+
+
+
+  )
+}
+
+export default Header
--- MarketFx-Client/src/screens/home.js
+++ MarketFx-Client/src/screens/home.js
@@ -7,9 +7,11 @@ import styled from "styled-components";
 import NewsComponent from "../components/NewsComponent";
 import AppButton from "../components/AppButton";
 import AppFooter from "../components/AppFooter";
+
 import NewsData from "../components/NewsData";
 import { useEffect, useState } from "react";
 import { NewsApi } from "../Api/NewsApi";
+import Header from "../components/Header";
 
 const newsList = [
   {
@@ -211,32 +213,8 @@ const Home = () => {
   }, [])
   return (
     <div>
-      <div className="Header">
-        <div className="Logo">
-          <p>MarketFx</p>
-        </div>
-        <div className="func">
-          <div className="search">
-            <InputGroup className="mb-3">
-              <Form.Control
-                placeholder="Recipient's username"
-                aria-label="Recipient's username"
-                aria-describedby="basic-addon2"
-              />
-              <InputGroup.Text id="basic-addon2">
-                <FaSearch />{" "}
-              </InputGroup.Text>
-            </InputGroup>
-          </div>
-          <div className="logbuttons">
-            <button className="SignUp btn btn-primary mr-4">SignUp</button>
-            <button className="Login btn btn-outline-primary ml-5">
-              Login
-            </button>
-          </div>
-        </div>
-      </div>
-      <nav></nav>
+      
+      <Header />
 
       <Container>
         <h2>Market News</h2>
